f64f47e8a8e5c2a8c0fa63b8bd4d82951e47dc17
yondav <yonidavid.abr@gmail.com>
2021-03-25 14:17:21 -0400

    working on popup bio

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,15 @@ const logo = document.querySelector('.logo');
 const logoYon = document.querySelector('.yon');
 const logoDav = document.querySelector('.dav');
 const contrast = document.querySelector('.contrast');
+const icons = document.querySelectorAll('.icon');
+const aboutBtn = document.querySelector('.about-btn');
+const chevron = document.querySelector('.fa-chevron-right');
+const popUp = document.querySelector('.pop-up');
 
 let ready = true;
 
+// event listeners for header
+// logo animation
 header.addEventListener('mousemove', (e) => {
   let mouseX = e.clientX;
   let centerX = e.currentTarget.offsetWidth / 2;
@@ -16,18 +22,59 @@ header.addEventListener('mousemove', (e) => {
   logoColors(logoYon, logoDav);
 });
 
+// defaults style to hardcoded when mouse leaves header
 header.addEventListener('mouseleave', () => {
   logo.style = '';
   logoYon.style = '';
   logoDav.style = '';
 });
 
+// toggle day night and changes icon
 contrast.addEventListener('click', (e) => {
   console.log('you called?');
-  e.preventDefault();
   body.classList.toggle('dark');
+  if (body.classList.contains('dark')) {
+    contrast.innerHTML = `<i class="fas fa-moon"></i>`;
+  } else {
+    contrast.innerHTML = `<i class="fas fa-sun"></i>`;
+  }
 });
 
+// mouseover for about btn
+aboutBtn.addEventListener('mouseover', () => {
+  aboutBtn.style.color = 'rgb(215, 186, 125)';
+  chevron.style.color = 'rgb(155, 220, 254)';
+  chevron.classList.remove('hide');
+  aboutBtn.style.transform = `scale(1.1)`;
+});
+
+aboutBtn.addEventListener('mouseleave', () => {
+  chevron.classList.add('hide');
+  aboutBtn.style.transform = '';
+  aboutBtn.style.color = '';
+});
+
+// toggle pop-up bio/contact
+aboutBtn.addEventListener('click', () => {
+  popUp.classList.add('slide');
+  let close = document.querySelector('.close');
+  close.addEventListener('click', () => {
+    popUp.classList.remove('slide');
+  });
+});
+
+// triggers bounce animation on everything in icon class
+icons.forEach((icon) => {
+  icon.addEventListener('mouseenter', () => {
+    icon.classList.add('bounce');
+  });
+  icon.addEventListener('animationend', (e) => {
+    icon.classList.remove('bounce');
+  });
+});
+
+// functions
+// generates random hexcode for changing logo colors
 function logoColors(x, y) {
   let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   let hexColor = '#'; // declares a block variable (let) so that there is always a # at the start of the hex color
