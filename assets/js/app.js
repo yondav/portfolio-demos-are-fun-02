6eadce170d1cd8f2e05a03889ba727018deba5ca
yondav <yonidavid.abr@gmail.com>
2021-03-24 22:37:37 -0400

    playing with mouse movement making logo translate

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,49 @@
 const header = document.querySelector('header');
+const logo = document.querySelector('.logo');
 const logoYon = document.querySelector('.yon');
 const logoDav = document.querySelector('.dav');
 
-header.addEventListener('mousemove', () => {
-  setTimeout(() => {
-    logoColors(logoYon);
-    logoColors(logoDav);
-  }, 1500);
+let ready = true;
+
+header.addEventListener('mousemove', (e) => {
+  let mouseY = e.clientY;
+  let mouseX = e.clientX;
+  let transArr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+  let randomTrans = transArr[Math.floor(Math.random() * transArr.length)];
+  logoColors(logoYon, logoDav);
+  if (mouseY && mouseX) {
+    logo.style.transform = `translate(${randomTrans}%, ${randomTrans}%)`;
+    console.log(randomTrans);
+  }
+  // if (mouseY < 288 || mouseX < 725) {
+  //   logo.style.transform = `translate(${randomTrans}%, ${randomTrans}%)`;
+  //   console.log(randomTrans);
+  // }
 });
 
-function logoColors(x) {
+header.addEventListener('mouseleave', () => {
+  logoYon.style = '';
+  logoDav.style = '';
+});
+
+// header.addEventListener('mouseup', () => {
+//   logo.style.background = 'white';
+// });
+
+function logoColors(x, y) {
   let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   let hexColor = '#'; // declares a block variable (let) so that there is always a # at the start of the hex color
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[Math.floor(Math.random() * hex.length)]; // for loop says that until the color code starts at 0 characters and adds a character until it reaches 6 characters long. The characters are defined to be a character from the hex variable declared on line 1. += makes it so that we are compounding a string rather than overriding the value of hex by using only =
+  if (ready === true) {
+    for (let i = 0; i < 6; i++) {
+      let xHex = (hexColor += hex[Math.floor(Math.random() * hex.length)]);
+      let yHex = (hexColor += hex[Math.floor(Math.random() * hex.length)]);
+      x.style.color = xHex;
+      y.style.color = yHex;
+    }
+
+    ready = false;
+    setTimeout(() => {
+      ready = true;
+    }, 800);
   }
-  x.style.color = hexColor;
 }
